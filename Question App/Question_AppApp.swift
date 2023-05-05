bbfb57d800bd2ce9fb3d3a8a98ffcded2b47c9a2
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2023-05-05 01:36:08 -0400

    Initial Commit

---
+++ Question App/Question_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Question_AppApp.swift
+//  Question App
+//
+//  Created by Kiara on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Question_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
